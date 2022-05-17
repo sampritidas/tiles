dfd1bf2798f259476f65d160605594b8acd4e039
Sampriti Das <99536474+sampritidas@users.noreply.github.com>
2022-05-17 11:21:49 +0530

    Modified bash script and changed data.txt to data.json

--- tiles.js
+++ tiles.js
@@ -1,23 +1,31 @@
+/* eslint-disable max-statements */
+/* eslint-disable no-console */
 const fs = require('fs');
 
-const createHTML = require('./createhtml.js').createHTML;
+const lib = require('./createhtml.js');
+const createHTML = lib.createHTML;
+const ONE = 1;
+const TWO = 2;
 
 const topElement = function (stack) {
-  return stack[stack.length - 1];
+  console.log(stack);
+  return stack[stack.length - ONE];
 };
 
 const tiles = function (block, firstChoice, secondChoice) {
-  const data = JSON.parse(fs.readFileSync('./data.txt', 'utf8'));
-  const topOfFirst = topElement(data[firstChoice - 1]);
-  const topOfSecond = topElement(data[secondChoice - 1]);
+  const data = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+  console.log(data);
+  createHTML(data);
+  const topOfFirst = topElement(data[firstChoice - ONE]);
+  const topOfSecond = topElement(data[secondChoice - ONE]);
   if (topOfFirst !== topOfSecond) {
     console.log('Pattern not matched');
-    return 1;
+    return;
   }
-  data[firstChoice-1].pop();
-  data[secondChoice-1].pop();
+  data[firstChoice - ONE].pop();
+  data[secondChoice - ONE].pop();
   createHTML(data);
-  fs.writeFileSync('data.txt', JSON.stringify(data), 'utf8');
+  fs.writeFileSync('data.json', JSON.stringify(data), 'utf8');
 };
 
-tiles(...process.argv.slice(2));
+tiles(...process.argv.slice(TWO));
--- tiles.sh
+++ tiles.sh
@@ -1,16 +1,22 @@
 function main() {
   local block=$1
-  local total_chance=$(($block*2));
+  local total_chance=$(($block*2))
   local chance=0
+
   while [[ $chance -lt $total_chance ]]
   do
-    echo "chance" $(($chance + 1))
     read -p "Enter first choice : " first
     read -p "Enter second choice : " second
-    node tiles.js $block $first $second
+    value=$( node tiles.js $block $first $second )
+    
+    if [[ $value == "Pattern not matched" ]]
+    then
+      continue
+    fi
+
     open tiles.html
     chance=$(($chance + 1))
   done
 }
 
-main
+main $1
